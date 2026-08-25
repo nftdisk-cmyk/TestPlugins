a37a7c4f2502564384f67f85524241bee04cc2d4
nftdisk-cmyk <nftdisk@gmail.com>
2026-08-25 13:59:20 +0300

    Change root project name from 'CloudstreamPlugins' to 'bushidoxyz'

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "CloudstreamPlugins"
+rootProject.name = "bushidoxyz"
 
 // This file sets what projects are included.
 // All new projects should get automatically included unless specified in the "disabled" variable.
@@ -16,4 +16,4 @@ fun File.eachDir(block: (File) -> Unit) {
 }
 
 // To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
-// include("PluginName")
+// include("PluginName") 
