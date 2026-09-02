b7e7529092abfcc1ba99596e390317e4111c39be
Bushidoxyz <mutlushenolov@gmail.com>
2026-09-02 11:28:17 +0300

    Bump to v16 - Patron Sports JSON API provider

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
 }
 
 // Sürüm numarasını tam sayı (Integer) olarak belirtiyoruz
-version = 15
+version = 16
 
 cloudstream {
     // Eklentimizin uygulamanın uzantılar sayfasında görünecek bilgileri
