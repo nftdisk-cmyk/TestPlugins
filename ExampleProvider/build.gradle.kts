9556e10778fb21e357eed3a3236f6c9d448835a4
Bushidoxyz <mutlushenolov@gmail.com>
2026-09-02 11:13:56 +0300

    Bump plugin version to 15 to trigger Cloudstream update

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -4,7 +4,7 @@ dependencies {
 }
 
 // Sürüm numarasını tam sayı (Integer) olarak belirtiyoruz
-version = 14
+version = 15
 
 cloudstream {
     // Eklentimizin uygulamanın uzantılar sayfasında görünecek bilgileri
